def51db2a9c2c84a151ea3e8ab846402d9339071
JLTurner310 <jlturner829@gmail.com>
2014-11-12 09:26:03 -0500

    Map (not working, app will be broken)

---
+++ ErrandsApp/app/src/main/java/errandsapp/errandsapp/Map.java
@@ -0,0 +1,48 @@
+package errandsapp.errandsapp;
+
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by JenniferTurner on 11/11/14.
+ */
+public class Map extends FragmentActivity {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_map, container, false);
+    }
+    import android.support.v4.app.FragmentActivity;
+    import android.os.Bundle;
+
+    import com.google.android.gms.maps.GoogleMap;
+    import com.google.android.gms.maps.SupportMapFragment;
+    import com.google.android.gms.maps.model.LatLng;
+    import com.google.android.gms.maps.model.MarkerOptions;
+
+    public class MapDemoActivity extends FragmentActivity {
+
+        private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_map_demo);
+            setUpMapIfNeeded();
+
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+
+    }
+}
