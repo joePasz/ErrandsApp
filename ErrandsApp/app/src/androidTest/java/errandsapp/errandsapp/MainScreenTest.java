6ca788084be3ef4f7befa57d448d8f2fc574a092
schuster110 <schuster110@Andreass-MacBook-Pro.local>
2014-11-30 22:21:48 -0500

    Test Cases Added
    Upload the first 2 cases I made. They are the basics and we can just
    add on from here.

---
+++ ErrandsApp/app/src/androidTest/java/errandsapp/errandsapp/MainScreenTest.java
@@ -0,0 +1,54 @@
+package errandsapp.errandsapp;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import junit.framework.Assert;
+import com.robotium.solo.Solo;
+
+import errandsapp.errandsapp.BuildRoute;
+import errandsapp.errandsapp.MainScreen;
+
+/**
+ * Created by schuster110 on 11/30/14.
+ */
+public class MainScreenTest extends ActivityInstrumentationTestCase2<MainScreen> {
+
+    private Solo solo;
+    private ImageButton img;
+
+    public MainScreenTest(){
+
+        super(MainScreen.class);
+    }
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+    }
+
+   public void testActivityActive() throws Exception {
+       solo.assertCurrentActivity("Wrong Activity", MainScreen.class);
+       solo.clickOnButton(solo.getString(R.string.build_route_button));
+      // solo.assertCurrentActivity("Wrong Activity",BuildRoute.class);
+   }
+
+    /** Testing to see if each button the screen is present
+     *
+     */
+    public void testButtonsPresent() {
+        solo.searchButton(solo.getString(R.string.build_route_button), true);
+        solo.searchButton(solo.getString(R.string.search_button),true);
+        solo.searchButton(solo.getString(R.string.GPSLoc),true);
+    }
+
+}
